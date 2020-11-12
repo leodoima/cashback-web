016db562dc0a8554b3c1ed7841d389e0b43d33d5
Leonardo Santos <leodoima@hotmail.com>
2020-11-11 21:03:59 -0300

    Criacao de menu lateral

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { FiHome, FiActivity, FiCreditCard, FiRepeat,FiUsers, FiLogOut, FiFileText, FiShoppingCart } from 'react-icons/fi';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+import { Container, Menu, Content } from './styles';
+import logoMova from '../../assets/logo.svg';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Menu>
+        <img src={logoMova} alt=""/>
+        <FiHome />
+        <FiActivity />
+        <FiCreditCard />
+        <FiRepeat />
+        <FiFileText />
+        <FiUsers />
+        <FiShoppingCart />
+        <FiLogOut />
+      </Menu>
+      <Content></Content>
+    </Container>
+  );
+}
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+  height: 100vh;
+  display: flex; 
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+
+  margin: 50px;
+  width: 100px;
+  height: 800px;
+
+  background: #091929;
+  border-radius: 15px;
+
+  img {
+    margin-top: 30px;
+    margin-bottom: 35px;
+  }
+
+  svg {
+    color: #fff;
+    width: 25px;
+    height: 25px;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#fff')};
+    }
+  }
+
+  svg:last-child  {
+    margin: 50px;
+    color: #ED8975;
+    transition: color 0.2s;
+
+    &:hover {
+      color: ${shade(0.2, '#ED8975')};
+    }
+  }
+`;
+
+export const Content = styled.div``;
